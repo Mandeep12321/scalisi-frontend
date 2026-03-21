0efe1947628a29064ab94c09b7127bb59cc3989f
Sumit Kumar <sumit.kumar@itechnolabs.tech>
2026-03-21 06:04:00 +0530

    added new changes

--- next.config.mjs
+++ next.config.mjs
@@ -4,17 +4,17 @@ const nextConfig = {
   reactStrictMode: false,
   eslint: { ignoreDuringBuilds: true },
   images: {
+    unoptimized: true, // ✅ ADD THIS LINE
+
     remotePatterns: [
       {
         protocol: "https",
         hostname: "scalisi-produce.s3.us-east-2.amazonaws.com",
       },
-
       {
         protocol: "https",
         hostname: "sskw3ghk-3040.inc1.devtunnels.ms",
       },
-
       {
         protocol: "https",
         hostname: "scalisi-backend-dd-414f134a66de.herokuapp.com",
