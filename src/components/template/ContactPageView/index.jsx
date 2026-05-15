926105d0aa60dec2db8fcb223a36e084da061c42
Rahul Nanera <rahulnanera203@gmail.com>
2026-05-15 21:23:00 +0530

    Update contact ( more changes ) section

--- src/components/template/ContactPageView/index.jsx
+++ src/components/template/ContactPageView/index.jsx
@@ -202,28 +202,6 @@ export default function ContactPageView({ cmsData }) {
           </Container>
         </div>
       </div>
-      <div className={classes.announcementSection}>
-        <Container>
-          <Row className="g-0">
-            <Col md={6} lg={6}>
-              <div className={mergeClass(classes.announcementLeft)}>
-                <AnnouncementCard
-                  data={_cmsData?.support || ABOUT_US_PAGE_DATA?.announcement1}
-                />
-              </div>
-            </Col>
-            <Col md={6} lg={6}>
-              <div className={mergeClass(classes.announcementRight)}>
-                <AnnouncementCard
-                  data={_cmsData?.updates || ABOUT_US_PAGE_DATA?.announcement2}
-                  placeholder="Email address"
-                  hasNewsletter={true}
-                />
-              </div>
-            </Col>
-          </Row>
-        </Container>
-      </div>
     </>
   );
 }
